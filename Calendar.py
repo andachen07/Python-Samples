2e604494992b5954ae63b47c9dfe2a96f6a41ae6
AndaChen <andachen07@gmail.com>
2021-02-19 16:41:33 +0800

    [python] Change List - use for loop to replace if...else...

--- Calendar.py
+++ Calendar.py
@@ -11,184 +11,20 @@ totalDays=int(year1*365+year1/4+year1/400-year1/100);
 if year%4==0:
       days[1]=29
 
-if month>1:
-   totalDays=totalDays+days[0]
-if month>2:     
-   totalDays=totalDays+days[1]
-if month>3:
-   totalDays=totalDays+days[2]      
-if month>4:     
-   totalDays=totalDays+days[3]
-if month>5:
-   totalDays=totalDays+days[4]
-if month>6:
-   totalDays=totalDays+days[5]
-if month>7:
-   totalDays=totalDays+days[6]
-if month>8:
-   totalDays=totalDays+days[7]
-if month>9:
-   totalDays=totalDays+days[8]
-if month>10:
-   totalDays=totalDays+days[9]      
-if month>11:
-   totalDays=totalDays+days[10]      
-if month>12:
-   totalDays=totalDays+days[11]      
- 
+for i in range(month-1):
+    totalDays=totalDays+days[i]
+print(days[1]) 
 iCount = 0
 print ("日\t一\t二\t三\t四\t五\t六")
+for i in range(int(totalDays)%7+1):
+    print ('\t', end=""),
+    iCount+=1
+for i in range(1,days[month-1]+1):
+    print ("%.2i\t" %i, end=""),
+    iCount +=1
+    if iCount%7 == 0 :
+      print ('')
 
-daynum=totalDays%7+1
 
-if daynum>0:
-   print ('\t', end=""); iCount+=1
-if daynum>1:
-   print ('\t', end=""); iCount+=1
-if daynum>2:
-   print ('\t', end=""); iCount+=1
-if daynum>3:
-   print ('\t', end=""); iCount+=1
-if daynum>4:
-   print ('\t', end=""); iCount+=1
-if daynum>5:
-   print ('\t', end=""); iCount+=1
-if daynum>6:
-   print ('\t', end=""); iCount+=1
-  
-i=1
-if (iCount%7==0):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>1):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>2):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>3):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>4):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>5):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>6):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7==0):
-    print ("")
-  
-if (iCount%7==0):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=1):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=2):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=3):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=4):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=5):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=6):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7==0):
-    print ("")
 
-if (iCount%7==0):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=1):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=2):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=3):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=4):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=5):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=6):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7==0):
-    print ("")
 
-if (iCount%7==0):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=1):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=2):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=3):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=4):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=5):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=6):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7==0):
-    print ("")
-
-if  iCount%7==0 and i<=days[month-1]:
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if  iCount%7>=1 and i<=days[month-1]:
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=2 and i<=days[month-1]):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=3 and i<=days[month-1]):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=4 and i<=days[month-1]):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=5 and i<=days[month-1]):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7>=6 and i<=days[month-1]):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if (iCount%7==0):
-    print ("")
-    
-if (iCount%7==0 and i<=days[month-1]):
-    print ("%.2i\t" %i, end="")
-    iCount+=1; i+=1
-if  iCount%7==1 and i<=days[month-1]:
-    print ("%.2i\t" %i, end="")
-if  iCount%7==2 and i<=days[month-1]:
-    print ("%.2i\t" %i, end="")
-if  iCount%7==3 and i<=days[month-1]:
-    print ("%.2i\t" %i, end="")
-if  iCount%7==4 and i<=days[month-1]:
-    print ("%.2i\t" %i, end="")
-if  iCount%7==5 and i<=days[month-1]:
-    print ("%.2i\t" %i, end="")
-if  iCount%7==6 and i<=days[month-1]:
-    print ("%.2i\t" %i, end="")
